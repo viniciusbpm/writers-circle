f69573ed93eed04f98dee3f72518383cbdb02d46
Vinícius <viniciusmpacheco22@gmail.com>
2023-09-22 08:27:46 -0300

    adding book controller and book & page domain

---
+++ api/src/main/java/br/ifsul/writerscircle/controller/ObraController.java
@@ -0,0 +1,10 @@
+package br.ifsul.writerscircle.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("obras")
+public class ObraController {
+
+}
